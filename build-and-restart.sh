80eba23e9066343ee9327dfd855bc658f7f5ba79
Sebastian Klimas <sebastianklimas@op.pl>
2024-04-09 23:10:33 +0200

    Ready for deployment

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
-docker build . -t Portfolio
-docker stop Portfolio || true
-docker rm Portfolio || true
-docker run -d -p 8080:8080 --name=Portfolio -e SPRING_PROFILES_ACTIVE=prod --network Portfolio-network --restart unless-stopped Portfolio
+docker build . -t portfolio
+docker stop portfolio || true
+docker rm portfolio || true
+docker run -d -p 8080:8080 --name=portfolio -e SPRING_PROFILES_ACTIVE=prod --network portfolio-network --restart unless-stopped portfolio
