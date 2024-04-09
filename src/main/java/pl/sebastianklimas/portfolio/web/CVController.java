cf66b7205ac965e4b654e78fc8bae303e042d550
Sebastian Klimas <sebastianklimas@op.pl>
2024-04-09 23:47:04 +0200

    CV flow rework

--- src/main/java/pl/sebastianklimas/portfolio/web/CVController.java
+++ src/main/java/pl/sebastianklimas/portfolio/web/CVController.java
@@ -1,32 +1,40 @@
 package pl.sebastianklimas.portfolio.web;
 
-import org.springframework.core.io.InputStreamResource;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 
 @Controller
 public class CVController {
 
     @GetMapping(path = "/cv")
-    public ResponseEntity<Resource> downloadFile() throws Exception {
-        File downloadFile = new File("/opt/Portfolio/files/CVKlimasSebastian.pdf");
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(downloadFile));
-        HttpHeaders header = new HttpHeaders();
-        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + downloadFile.getName());
-        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        header.add("Pragma", "no-cache");
-        header.add("Expires", "0");
-        return ResponseEntity.ok()
-                .headers(header)
-                .contentLength(downloadFile.length())
-                .contentType(MediaType.parseMediaType("application/octet-stream"))
-                .body(resource);
+    public void downloadFile2(HttpServletResponse response) {
+        try {
+            Resource resource = new ClassPathResource("files/CVKlimasSebastian.pdf");
+
+            response.setContentType("application/pdf");
+
+            response.setHeader("Content-Disposition", "attachment; filename=" + resource.getFilename());
+
+            InputStream inputStream = resource.getInputStream();
+
+            OutputStream outputStream = response.getOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            inputStream.close();
+            outputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
